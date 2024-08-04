56cec8e2a0e3e8d0b46fb1f5b743246f400b5ebb
sanjauBeni <sanjaysewa999@gmail.com>
2024-08-04 21:41:28 +0530

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavBar from "./componetns/ui_components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <NavBar/>
+        <div className="container">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, ipsum explicabo ullam aspernatur nobis, corporis sapiente nemo recusandae dolor voluptatibus sint, enim pariatur. Repellendus commodi dicta corporis fugiat rem tempore!</p>
+        </div>
+    </>
   );
 }
 
